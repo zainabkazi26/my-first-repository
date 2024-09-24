8a12155affb6c3361787f92fa2475e8419080342
zainab <zainabkazi26@gmail.com>
2024-09-24 12:59:54 +0530

    initial commit

---
+++ src/main/java/com/eng/anglo/AngloApplication.java
@@ -0,0 +1,13 @@
+package com.eng.anglo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eng/anglo/AngloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eng.anglo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
